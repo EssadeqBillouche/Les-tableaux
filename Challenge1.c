0010afcd4e3743ab7327923618ce162118660f1e
MrNobodyCode <essadeq.billouche@gmail.com>
2024-09-03 18:27:18 -0400

    Create Challenge1.c

---
+++ Challenge1.c
@@ -0,0 +1,12 @@
+#include"stdio.h"
+
+main(void)
+{
+        int i;
+        int Tab[5] = {11, 12, 13, 14, 15};
+        for (i = 0; i < 5; i++)
+        {
+                printf(" Le normber No %d est %d \n", i+1, Tab[i]);
+        }
+        return 0;
+}
